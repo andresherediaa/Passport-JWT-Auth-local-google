dc0df350b043c95832e729c78dce04e7ac9ce3ef
ANDRES HEREDIA <31019836+andresherediaa@users.noreply.github.com>
2022-12-03 22:28:21 -0500

    add validation for login with same email as goolge account

--- config/passport-local-setup.js
+++ config/passport-local-setup.js
@@ -12,15 +12,13 @@ passport.use(
 
         if (currentUser.source != "local") {
             return done(
-                new Error(
-                    `You have previously signed up with a different signin method`
-                )
+                new Error(`You have previously signed up with Google account`)
             );
         }
         if (!bcrypt.compareSync(password, currentUser.password)) {
             return done(new Error("Incorrect password provided"));
         }
-        console.log("login con registrered user", currentUser)
+        console.log("login con registrered user", currentUser);
         return done(null, currentUser);
     })
 );
